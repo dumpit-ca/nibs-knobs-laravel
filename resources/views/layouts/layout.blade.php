5ac276679ed6cad9e71c8216cdbde2df33fa2e38
dumpit-ca <dumpitca@outlook.com>
2023-01-12 13:27:01 +0800

    added title page

--- resources/views/layouts/layout.blade.php
+++ resources/views/layouts/layout.blade.php
@@ -1,16 +1,59 @@
 <!DOCTYPE html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-    <head>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <title>Laravel</title>
-        <link rel="stylesheet" href="/css/main.css">
-        <!-- Fonts -->
-        <link href="https://fonts.googleapis.com/css2?family=Nunito:wght@200;600&display=swap" rel="stylesheet">
+<head>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" href="/css/main.css">
+  {{-- Favicon --}}
+  <link rel="shortcut icon" href="/images/n&k-icon.png" type="image/x-icon">
+  {{-- Icons --}}
+  <script src="https://kit.fontawesome.com/1d56e9de16.js" crossorigin="anonymous"></script>
+  <title>N&K | @yield('title')</title>
+</head>
 
-    </head>
-    <body>
-        @yield('content')
-    </body>
+<body>
+  <nav class="navbar navbar-expand-lg px-3 d-flex align-content-center">
+    <div class="container-fluid">
+      <a href="{{ url('home') }} class="navbar-brand"><img src="/images/nk-logo.png" alt=""></a>
+      <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false"
+        aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="collapse navbar-collapse" id="navbarSupportedContent">
+        <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+          <li class="nav-item">
+            <a href="{{ url('home') }}" class="nav-link active" aria-current="page" ">
+              <i class="fa-solid fa-house me-1"></i>
+              Home</a>
+          </li>
+          <li class="nav-item dropdown">
+            <a href="#" class="nav-link dropdown-toggle"  role="button" data-bs-toggle="dropdown" aria-expanded="false">
+              <i class="fa-solid fa-compass me-1"></i>
+              Explore
+            </a>
+            <ul class="dropdown-menu">
+              <li><a href="#"class="dropdown-item" >Gallery</a></li>
+              <li><a href="#" class="dropdown-item" >Kitchen</a></li>
+              <li><a href="#" class="dropdown-item">Bedroom</a></li>
+              <li><a href="#" class="dropdown-item">Living Room</a></li>
+              <li><a href="#" class="dropdown-item">Bathroom</a></li>
+              <li><a href="#" class="dropdown-item">Space Saving</a></li>
+              <li><a href="#" class="dropdown-item">Home Office</a></li>
+            </ul>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link disabled">Disabled</a>
+          </li>
+        </ul>
+        <button class="btn btn-outline-success" type="submit">Search</button>
+      </div>
+    </div>
+  </nav>
+  <div class="container-fluid">
+    @yield('content')
+  </div>
+  <script src="/js/app.js"></script>
+</body>
+
 </html>
