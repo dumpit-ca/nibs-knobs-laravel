7b01fbd4a75059747aa72711a6e97b888bafed84
dumpit-ca <dumpitca@outlook.com>
2023-01-14 02:32:00 +0800

    added slick-carousel and jquery package

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,4 +1,6 @@
+window.$ = window.jQuery = window.jquery = require("jquery");
 require("./bootstrap");
 import "bootstrap";
 // SweetAlert2
 window.Swal = require("sweetalert2/dist/sweetalert2.all.min");
+require("slick-carousel");
