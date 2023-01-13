7ecfba820e676100d4b15df3e87e0d367bdf4904
dumpit-ca <dumpitca@outlook.com>
2023-01-13 13:02:21 +0800

    added controller for Consultation

--- routes/web.php
+++ routes/web.php
@@ -10,7 +10,11 @@
 | contains the "web" middleware group. Now create something great!
 |
 */
-
+Route::get('/', function () {
+    return redirect('/home');
+});
 Route::get('/home', function () {
     return view('pages.home');
 });
+Route::get('/consult', 'ConsultationController@index');
+Route::get('/contact', 'ContactController@index');
