713d6c1fe02934534706198a9f403f274352a1f9
dumpit-ca <dumpitca@outlook.com>
2023-01-15 14:43:50 +0800

    added routes for home

--- routes/web.php
+++ routes/web.php
@@ -13,9 +13,7 @@
 Route::get('/', function () {
     return redirect('/home');
 });
-Route::get('/home', function () {
-    return view('pages.home');
-});
+Route::get('/home', 'HomeController@index')->name('home');
 Route::get('/consult', 'ConsultationController@index');
 Route::get('/contact', 'ContactController@index');
 Route::get('/gallery', 'GalleryController@index');
@@ -25,4 +23,4 @@ Route::get('/gallery/living-room', 'LivingRoomController@index')->name('livingro
 Route::get('/gallery/bathroom', 'BathroomController@index')->name('bathroom');
 Route::get('/gallery/space-saving', 'SpaceSavingController@index')->name('spacesaving');
 Route::get('/gallery/home-office', 'HomeOfficeController@index')->name('homeoffice');
-// Route::get('/login', 'LoginController@index');
+Auth::routes();
