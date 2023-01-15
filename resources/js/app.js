f97c131eab6bf6359e9b6f19f153104e05210c03
dumpit-ca <dumpitca@outlook.com>
2023-01-16 00:06:06 +0800

    required bootstrap

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,6 +1,6 @@
 window.$ = window.jQuery = window.jquery = require("jquery");
-require("./bootstrap");
-import "bootstrap";
+require("bootstrap");
+
 // SweetAlert2
 window.Swal = require("sweetalert2/dist/sweetalert2.all.min");
 require("slick-carousel");
