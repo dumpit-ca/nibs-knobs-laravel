f301cb2117cf19c8da5b6b38cfe8707b8bcbbf91
dumpit-ca <dumpitca@outlook.com>
2023-01-16 00:08:45 +0800

    added additional meta

--- resources/views/layouts/front.blade.php
+++ resources/views/layouts/front.blade.php
@@ -3,7 +3,7 @@
 
 <head>
   <meta charset="utf-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
   <link rel="stylesheet" href="{{ asset('/css/main.css') }}">
   {{-- Favicon --}}
   <link rel="shortcut icon" href="{{ asset('/images/n&k-icon.png') }}" type="image/x-icon">
