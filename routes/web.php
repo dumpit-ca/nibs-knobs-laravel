0a77d2fdf467c82cfed7e7a6953df14a7973e6c7
dumpit-ca <dumpitca@outlook.com>
2023-01-16 23:58:12 +0800

    created PageController

--- routes/web.php
+++ routes/web.php
@@ -10,17 +10,26 @@
 | contains the "web" middleware group. Now create something great!
 |
 */
+
+// Front
+Route::get('/home', 'PageController@index')->name('home');
 Route::get('/', function () {
     return redirect('/home');
 });
-Route::get('/home', 'HomeController@index')->name('home');
-Route::get('/consult', 'ConsultationController@index');
-Route::get('/contact', 'ContactController@index');
-Route::get('/gallery', 'GalleryController@index');
+Route::get('/consult', 'ConsultationController@index')->name('consult');
+Route::get('/contact', 'PageController@contact')->name('contact');
+Route::get('/gallery', 'GalleryController@index')->name('gallery');
 Route::get('/gallery/kitchen', 'KitchenController@index')->name('kitchen');
 Route::get('/gallery/bedroom', 'BedroomController@index')->name('bedroom');
 Route::get('/gallery/living-room', 'LivingRoomController@index')->name('livingroom');
 Route::get('/gallery/bathroom', 'BathroomController@index')->name('bathroom');
 Route::get('/gallery/space-saving', 'SpaceSavingController@index')->name('spacesaving');
 Route::get('/gallery/home-office', 'HomeOfficeController@index')->name('homeoffice');
+
+
+// Auth
 Auth::routes();
+
+
+// Admin
+Route::get('/dashboard', 'PageController@dashboard')->name('dashboard');
