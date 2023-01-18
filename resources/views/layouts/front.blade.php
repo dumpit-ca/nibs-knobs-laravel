38bac699a2eb190de60a06dca75b9efc60d09a60
dumpit-ca <dumpitca@outlook.com>
2023-01-18 16:56:17 +0800

    Finished homepage

--- resources/views/layouts/front.blade.php
+++ resources/views/layouts/front.blade.php
@@ -2,28 +2,26 @@
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
 
 <head>
-  <meta charset="utf-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-  <link rel="stylesheet" href="{{ asset('/css/main.css') }}">
-  {{-- Favicon --}}
-  <link rel="shortcut icon" href="{{ asset('/images/n&k-icon.png') }}" type="image/x-icon">
-  {{-- Icons --}}
-  <script src="https://kit.fontawesome.com/1d56e9de16.js" crossorigin="anonymous"></script>
-  <!-- CSRF Token -->
-  <meta name="csrf-token" content="{{ csrf_token() }}">
-  {{-- Title --}}
-  <title>N&K | @yield('title')</title>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <link rel="stylesheet" href="{{ asset('/css/main.css') }}">
+    {{-- Favicon --}}
+    <link rel="shortcut icon" href="{{ asset('/images/n&k-icon.png') }}" type="image/x-icon">
+    {{-- Icons --}}
+    <link href='https://unpkg.com/boxicons@2.0.7/css/boxicons.min.css' rel='stylesheet'>
+    <!-- CSRF Token -->
+    <meta name="csrf-token" content="{{ csrf_token() }}">
+    {{-- Title --}}
+    <title>N&K | @yield('title')</title>
 
 </head>
 
 <body>
-  @include('components.front.navbar')
-  @yield('content')
-  {{-- JQuery --}}
-  {{-- <script src="https://code.jquery.com/jquery-3.6.1.min.js"></script> --}}
-  <script src="{{ asset('/js/app.js') }}"></script>
-  <script>
-    function bookedSuccess() {
+    @include('components.front.navbar')
+    @yield('content')
+    <script src="{{ asset('/js/app.js') }}"></script>
+    <script>
+        function bookedSuccess() {
       const Toast = Swal.mixin({
         toast: true,
         position: 'top-end',
@@ -60,8 +58,8 @@
         title: 'Message sent succesfully'
       })
     }
-  </script>
-  @yield('script')
+    </script>
+    @yield('script')
 </body>
 
 </html>
--- resources/views/pages/index.blade.php
+++ resources/views/pages/index.blade.php
@@ -1,205 +1,94 @@
 @extends('layouts.front')
 @section('title', 'Home')
 @section('content')
-  <div class="container">
-    <div class="row text-md-start text-center flex-column">
-      <div class="col-lg col-sm mt-4">
-        <h1 class="display-5 text-uppercase SoraBold">YOUR PERFECT HOME</h1>
-        <h1 class="display-5">
-          Right at your
-          <span class="RockSalt text-uppercase d-inline-block mt-lg-0 mt-4" id="fingertips">fingertips</span>
-        </h1>
-      </div>
-    </div>
-  </div>
-  {{-- about us --}}
-  <section class="container my-5">
-    <div class="row">
-      <div class="col col-lg-4 w-lg-50">
-        <div class="d-flex flex-column align-items-start flex-shrink-1">
-          <h2 class="display-6 SoraBold">What's about us?</h2>
-          <p class="fs-5 fw-normal" id="text">
-            We came up with a concept. In different probable house problems, many of us struggling with interior design of our homes. Therefore, we
-            created this website to provide a great solution for the people with the same issue. This website offers a variety of interior designs for
-            many types of set up for your home from tradition to latest design with efficiency, space-saving and aesthetic looking interior design etc.
-          </p>
-          <a class="btn fs-6 mb-4 rounded-pill px-5 py-1 text-uppercase mx-md-0 d-flex align-items-center">check our services</a>
+<div class="container-fluid px-4 py-md-5 py-0">
+    <div class="row row-cols-md-2 row-cols-1 py-5">
+        <div class="col-10 col-sm-8 col-lg-6">
+            <img src="/images/home-images/header.png" alt="" class="d-lg-block d-none mx-lg-auto img-fluid">
         </div>
-      </div>
-      <div class="col-md-5 col-12 d-flex flex-column gap-3 mb-3">
-        <img src="/images/home-images/home-1.png" alt="N&K chairs" />
-        <img src="/images/home-images/home-2.png" alt="N&K Sofa Set" />
-      </div>
-      <div class="col-md-3 col-sm-1 gap-5">
-        <img class="img-responsive" id="home-3" src="/images/home-images/home-3.png" alt="" />
-      </div>
-    </div>
-  </section>
-  {{-- N&K Culture --}}
-  <section class="container my-lg-5 my-3">
-    <h1 class="display-6 text-lg-start text-center SoraBold">The Nibs &amp; Knobs Culture</h1>
-    <!-- first row -->
-    <div class="row g-0 mt-4 mb-3 p-0">
-      <div class="col-md-8 ps-lg-2 ps-md-2 ps-0 mb-md-0 mb-3 culture-text position-relative">
-        <img src="/images/home-images/culture-section/culture-1.png" alt="" class="w-100 h-100" />
-        <div class="d-flex flex-column gap-1 RockSalt caption overflow-hidden">
-          <p class="fs-4 position-absolute mb-0" id="craft-caption">
-            we source furniture <br />
-            from the finest craftsmen
-          </p>
-        </div>
-      </div>
-      <div class="col-12 col-md-4 ps-lg-3 ps-md-3 ps-0 culture-text position-relative">
-        <img src="/images/home-images/culture-section/culture-2.png" alt="" class="w-100 h-100" />
-        <div class="d-flex flex-column gap-1 RockSalt caption overflow-hidden text-md-end text-start">
-          <p class="fs-4 me-0 ms-2 position-absolute mb-0" id="quality-caption">
-            quality is the <br />
-            aesthetic
-          </p>
-        </div>
-      </div>
-    </div>
-    <!-- second row -->
-    <div class="row g-0 mb-3 p-0">
-      <div class="col-sm ps-md-2 pb-md-0 pb-3 culture-text position-relative">
-        <img src="/images/home-images/culture-section/culture-3.png" alt="" class="w-100 h-100" />
-        <div class="d-flex flex-column gap-1 caption overflow-hidden">
-          <p class="fs-4 lh-sm ms-md-0 ms-0 mb-0 position-absolute text-uppercase text-start second-row-caption">
-            <span class="SoraBold">consult</span>
-            <br />
-            <span class="SoraBold">your designs</span>
-            <br />
-            easily
-          </p>
-        </div>
-      </div>
-      <div class="col-sm px-md-2 pb-md-0 pb-3 culture-text position-relative">
-        <img src="/images/home-images/culture-section/culture-4.png" alt="" class="w-100 h-100" />
-        <div class="d-flex flex-column gap-1 caption overflow-hidden">
-          <p class="fs-4 lh-sm ms-md-0 ms-0 mb-0 position-absolute text-uppercase text-start second-row-caption">
-            <span class="SoraBold">know</span>
-            <br />
-            <span class="SoraBold">your space</span>
-            <br />
-            <span>with us</span>
-          </p>
-        </div>
-      </div>
-      <div class="col-sm pe-md-0 pb-md-0 pb-3 culture-text position-relative">
-        <img src="/images/home-images/culture-section/culture-5.png" alt="" class="w-100 h-100" />
-        <div class="d-flex flex-column gap-1 caption overflow-hidden">
-          <p class="lh-sm ms-md-0 ms-0 mb-0 position-absolute text-uppercase text-start" id="beautify-caption">
-            <span class="SoraBold fs-4">big? small?</span>
-            <br />
-            <span class="fs-4">beautify your</span>
-            <br />
-            <span class="fs-4">home</span>
-          </p>
-        </div>
-      </div>
-    </div>
-    <!-- third row -->
-    <div class="row g-0 p-0">
-      <div class="col-12 col-md-4 ps-md-2 ps-0 mb-md-0 mb-3 culture-text position-relative">
-        <img src="/images/home-images/culture-section/culture-6.png" alt="" class="w-100 h-100" />
-        <div class="d-flex flex-column gap-1 RockSalt caption overflow-hidden">
-          <p class="fs-4 m-0 position-absolute third-row-caption">
-            your plan? <br />
-            we'll work out
-          </p>
-        </div>
-      </div>
-      <div class="col-md-8 ps-md-3 ps-0 culture-text position-relative">
-        <img src="/images/home-images/culture-section/culture-7.png" alt="" class="w-100 h-100" />
-        <div class="d-flex flex-column gap-1 RockSalt caption overflow-hidden">
-          <p class="fs-4 m-0 position-absolute third-row-caption" id="nk-caption">
-            select your needs &amp; wants <br />
-            even your nibs and knobs!
-          </p>
-        </div>
-      </div>
-    </div>
-  </section>
-  <section class="container my-5">
-    <div class="text-md-start text-center my-5">
-      <h2 class="display-6 SoraBold">N&amp;K Services</h2>
-    </div>
-    <div class="row p-0 g-0">
-      <div class="col-md-8">
-        <div class="row row-cols-1 row-cols-md-2 g-4">
-          <div class="col">
-            <div class="services-card py-4 mb-3">
-              <div class="row g-0 align-items-center">
-                <div class="col-md-4">
-                  <img src="/images/home-images/services/icon-book.png" alt="N&K Booking icon" class="img-fluid mx-auto d-block" />
+        <div class="col-lg-6 d-flex flex-column">
+            <p class="subheader text-uppercase fs-5">ABOUT US</p>
+            <h1 class="display-6">
+                The
+                <span class="text-uppercase">NIBS &amp; KNOBS</span>
+                Culture: <br>
+                Get to know the hype in Interior Design
+            </h1>
+            <p class="fs-6">We came up with a concept.
+                In different probable house problems, many of us struggling with interior design of our homes.
+                Therefore, we created this website to provide a great solution for the people with the same issue.
+                This website offers a wide community to share insights, create forums on the wonders of Interior Design.
+            </p>
+            <div class="author-card border-0 mb-3" style="max-width: 276px">
+                <div class="d-flex align-items-center">
+                    <div class="me-3">
+                        <div class="profile-img"></div>
+                    </div>
+                    <div>
+                        <h5 class="fs-6 card-title lh-2">Cassandra Dumpit</h5>
+                        <p class="card-text lh-2">Lead Product Developer</p>
+                    </div>
                 </div>
-                <div class="col-md-8">
-                  <div class="card-body lh-sm mt-md-0 mt-4 text-md-start text-center">
-                    <p class="card-title fs-4">Book Design</p>
-                    <p class="card-title fs-4">Consultancy</p>
-                  </div>
-                </div>
-              </div>
             </div>
-          </div>
-          <div class="col">
-            <div class="services-card py-4 mb-3">
-              <div class="row g-0 align-items-center">
-                <div class="col-md-4">
-                  <img src="/images/home-images/services/icon-furniture.png" alt="N&K Furniture icon" class="img-fluid mx-auto d-block" />
-                </div>
-                <div class="col-md-8">
-                  <div class="card-body lh-sm mt-md-0 mt-4 text-md-start text-center">
-                    <p class="card-title fs-4">Source out</p>
-                    <p class="card-title fs-4">Furnitures</p>
-                  </div>
-                </div>
-              </div>
-            </div>
-          </div>
-          <div class="col">
-            <div class="services-card py-4 mb-3">
-              <div class="row g-0 align-items-center">
-                <div class="col-md-4">
-                  <img src="/images/home-images/services/icon-assessment.png" alt="N&K Assessment icon" class="img-fluid mx-auto d-block" />
-                </div>
-                <div class="col-md-8">
-                  <div class="card-body lh-sm mt-md-0 mt-4 text-md-start text-center">
-                    <p class="card-title fs-4">Space</p>
-                    <p class="card-title fs-4">Assessment</p>
-                  </div>
-                </div>
-              </div>
-            </div>
-          </div>
-          <div class="col">
-            <div class="services-card py-4 mb-3">
-              <div class="row g-0 align-items-center">
-                <div class="col-md-4">
-                  <img src="/images/home-images/services/icon-map.png" alt="" class="img-fluid mx-auto d-block" />
-                </div>
-                <div class="col-md-8">
-                  <div class="card-body mt-md-0 mt-4 text-md-start text-center">
-                    <p class="card-title lh-sm fs-4">Planning and</p>
-                    <p class="card-title lh-sm fs-4">Recommendation</p>
-                  </div>
-                </div>
-              </div>
-            </div>
-          </div>
         </div>
-      </div>
-      <div class="col-12 col-md-4 ps-md-3 ps-0">
-        <div class="fs-5 me-0 mb-0 my-md-0 my-4 text-start">
-          <p>What do you need help with? Here are the services offered by Nibs & Knobs.</p>
-          <br />
-          <p>
-            Nibs & Knobs offers different services designed to help customers. Whether its about planning the home interior design, to sourcing out the
-            right furnitures and materials, to assessing house spaces, and to asking a professional for a plan or recommendation, Nibs and Knobs got it
-            covered. If there are any suggestions, kindly send us a message.
-          </p>
-        </div>
-      </div>
     </div>
-  </section>
+</div>
+<div class="container-fluid px-4 py-md-5 py-0">
+    <h1 class="display-6">Check <span class="fw-bold">what's happening!</span></h1>
+    <div class="row row-cols-md-2 row-cols-1 py-3">
+        <div class="col-10 col-sm-8 col-lg-8">
+            <div class="recent-card card mb-3 p-3">
+                <div class="row g-0">
+                    <div class="col-md-3">
+                        <img src="/images/home-images/recent.png" class="img-fluid" alt="">
+                    </div>
+                    <div class="col-md-8">
+                        <div class="card-body">
+                            <div class="author-card border-0 mb-3" style="max-width: 276px">
+                                <div class="d-flex align-items-center">
+                                    <div class="me-3">
+                                        <div class="profile-img"></div>
+                                    </div>
+                                    <div class="profile">
+                                        <p class="fs-5 card-title m-0" id="profile-name">Jhon Louie <span
+                                                class="fs-6 fw-normal">@thejldeleon</span>
+                                        </p>
+                                        <p class="fs-6 card-text text-muted m-0">Few minutes ago</p>
+                                    </div>
+
+                                </div>
+                            </div>
+                            <p class="card-text">Just finished a home renovation and loving the new minimalistic
+                                interior design. Less is definitely more when it comes
+                                to creating a peaceful and stylish living space #interiordesign #homedecor #renovation
+                            </p>
+                            <a href="#" class="btn btn-comment">
+                                <i class='bx bx-message-rounded me-1 fs-6'></i>
+                                <span>Comment</span>
+                            </a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="col-md-10 mx-auto col-lg-4">
+            <form class="p-4 p-md-5 rounded-3 login-form" data-bitwarden-watching="1">
+                <h1 class="fs-2 fw-bold">Continue Exploring</h1>
+                <p class="fs-5 mb-2">Login your account now!</p>
+                <div class="form-floating mb-3">
+                    <input type="text" class="form-control" id="floatingInput" placeholder="Username">
+                    <label for="floatingInput">Username</label>
+                </div>
+                <div class="form-floating mb-3">
+                    <input type="password" class="form-control" id="floatingPassword" placeholder="Password">
+                    <label for="floatingPassword">Password</label>
+                </div>
+                <div class="d-grid gap-2 d-md-flex justify-content-md-start">
+                    <a href="#" class="btn btn-login px-5 w-100">Login Account</a>
+                    <a href="#" class="btn btn-register px-5 w-100">Register Account</a>
+                </div>
+            </form>
+        </div>
+    </div>
+</div>
 @endsection
