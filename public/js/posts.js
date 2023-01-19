5314542d95c1aef8571a7045467c7a1e1b9e658f
dumpit-ca <dumpitca@outlook.com>
2023-01-19 17:40:29 +0800

    added dropzonejs

---
+++ public/js/posts.js
@@ -0,0 +1,9 @@
+Dropzone.options.myGreatDropzone = {
+    paramName: "file", // The name that will be used to transfer the file
+    maxFilesize: 2, // MB
+    acceptedFiles: ".jpg, .png, .jpeg, .mp4, .mkv",
+    autoProcessQueue: false,
+    uploadMultiple: false,
+    dictDefaultMessage: "Drop a photo or click this section to Upload",
+    addRemoveLinks: true,
+};
--- resources/views/layouts/front.blade.php
+++ resources/views/layouts/front.blade.php
@@ -9,7 +9,6 @@
     <link rel="shortcut icon" href="{{ asset('/images/n&k-icon.png') }}" type="image/x-icon">
     {{-- Icons --}}
     <link href='https://unpkg.com/boxicons@2.0.7/css/boxicons.min.css' rel='stylesheet'>
-    {{-- Dropzone CSS --}}
     <link rel="stylesheet" href="https://unpkg.com/dropzone@5/dist/min/dropzone.min.css" type="text/css" />
     <!-- CSRF Token -->
     <meta name="csrf-token" content="{{ csrf_token() }}">
@@ -20,49 +19,12 @@
 
 <body>
     @include('components.front.navbar')
-    @yield('content')
     <script src="{{ asset('/js/app.js') }}"></script>
-    <script src="https://unpkg.com/dropzone@5/dist/min/dropzone.min.js"></script>
-    <script>
-        function bookedSuccess() {
-      const Toast = Swal.mixin({
-        toast: true,
-        position: 'top-end',
-        showConfirmButton: false,
-        timer: 3000,
-        timerProgressBar: true,
-        didOpen: (toast) => {
-          toast.addEventListener('mouseenter', Swal.stopTimer)
-          toast.addEventListener('mouseleave', Swal.resumeTimer)
-        }
-      })
-
-      Toast.fire({
-        icon: 'success',
-        title: 'Booked Succesfully'
-      })
-    }
-
-    function contactSuccess() {
-      const Toast = Swal.mixin({
-        toast: true,
-        position: 'top-end',
-        showConfirmButton: false,
-        timer: 3000,
-        timerProgressBar: true,
-        didOpen: (toast) => {
-          toast.addEventListener('mouseenter', Swal.stopTimer)
-          toast.addEventListener('mouseleave', Swal.resumeTimer)
-        }
-      })
-
-      Toast.fire({
-        icon: 'success',
-        title: 'Message sent succesfully'
-      })
-    }
-    </script>
+    @yield('content')
     @yield('script')
+    <script src="https://unpkg.com/dropzone@5/dist/min/dropzone.min.js"></script>
+    <script src="{{asset('/js/posts.js')}}"></script>
+
 </body>
 
 </html>
