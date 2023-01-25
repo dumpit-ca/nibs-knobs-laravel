8222fe02d342d06b6207a084f714e2ca0ce0cd6b
dumpit-ca <dumpitca@outlook.com>
2023-01-25 16:16:41 +0800

    fixed margins and width  of posts

--- resources/views/pages/posts.blade.php
+++ resources/views/pages/posts.blade.php
@@ -19,7 +19,7 @@
             </aside>
         </div>
         <div class="col-md-8">
-            <div class="create-posts p-4 mb-4">
+            <div class="create-posts p-4 mb-4 mt-md-0 mt-4">
                 <div class="d-flex align-items-center mb-3">
                     <div class="me-3">
                         <img src="{{asset('/images/img-placeholder.png')}}" alt="" class="profile-img-posts img-fluid">
@@ -42,13 +42,67 @@
                     <button class="btn btn-primary me-md-2 px-5" type="button">Post Entry</button>
                 </div>
             </div>
-            <div class="posts">
-                <img src="..." class="card-img-top" alt="...">
-                <div class="card-body">
-                    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the
-                        card's
-                        content.</p>
+
+            <div class="posts p-3 mb-4 mt-md-0 mt-4">
+                <div class="d-flex align-items-center mb-3">
+                    <div class="me-3">
+                        <img src="{{asset('/images/img-placeholder.png')}}" alt="" class="profile-img-posts img-fluid">
+                    </div>
+                    <div class="profile">
+                        <p class="fs-6 card-title m-0" id="profile-name">Jhon Louie <span
+                                class="fs-6 fw-normal">@thejldeleon</span>
+                        </p>
+                        <p class="fs-6 card-text text-muted m-0">will post something</p>
+                    </div>
                 </div>
+                <div class="card-body">
+                    <p class="card-title fs-5 mb-4">Title of post</p>
+                </div>
+                <img src="{{asset('/images/home-images/home-2.png')}}" class="img-fluid mb-4" alt="...">
+                <div class="card-body">
+                    <p class="card-text">
+                        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut
+                        labore et dolore magna
+                        aliqua. Duis at consectetur lorem donec. Dolor magna eget est lorem. Vel eros donec ac odio
+                        tempor. Volutpat odio
+                        facilisis mauris sit amet massa vitae tortor.
+                    </p>
+                </div>
+                <a href="#" class="btn btn-comment mt-3">
+                    <i class='bx bx-message-rounded me-1 fs-6'></i>
+                    <span class="fs-6 fw-normal">Comment</span>
+                </a>
+            </div>
+
+            <div class="posts p-3 mb-4">
+                <div class="d-flex align-items-center mb-3">
+                    <div class="me-3">
+                        <img src="{{asset('/images/img-placeholder.png')}}" alt="" class="profile-img-posts img-fluid">
+                    </div>
+                    <div class="profile">
+                        <p class="fs-6 card-title m-0" id="profile-name">Jhon Louie <span
+                                class="fs-6 fw-normal">@thejldeleon</span>
+                        </p>
+                        <p class="fs-6 card-text text-muted m-0">will post something</p>
+                    </div>
+                </div>
+                <div class="card-body">
+                    <p class="card-title fs-5 mb-4">Title of post</p>
+                </div>
+                <img src="{{asset('/images/home-images/home-2.png')}}" class="img-fluid mb-4 mx-auto d-block" alt="...">
+                <div class="card-body">
+                    <p class="card-text">
+                        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut
+                        labore et dolore magna
+                        aliqua. Duis at consectetur lorem donec. Dolor magna eget est lorem. Vel eros donec ac odio
+                        tempor. Volutpat odio
+                        facilisis mauris sit amet massa vitae tortor.
+                    </p>
+                </div>
+                <a href="#" class="btn btn-comment mt-3">
+                    <i class='bx bx-message-rounded me-1 fs-6'></i>
+                    <span class="fs-6 fw-normal">Comment</span>
+                </a>
             </div>
         </div>
     </div>
--- resources/views/pages/profile.blade.php
+++ resources/views/pages/profile.blade.php
@@ -18,185 +18,95 @@
             </aside>
         </div>
         <div class="col-md-8 overflow-auto">
-            <div class="posts-card card mb-3">
-                <div class="row g-0">
-                    <div class="col-md-3 p-3 my-auto">
-                        <img src="{{asset('/images/home-images/home-1.png')}}" alt="" class="img-fluid">
+            <div class="posts p-3 mb-4 mt-md-0 mt-4">
+                <div class="d-flex align-items-center mb-3">
+                    <div class="me-3">
+                        <img src="{{asset('/images/img-placeholder.png')}}" alt="" class="profile-img-posts img-fluid">
                     </div>
-                    <div class="col-md-7">
-                        <div class="card-body">
-                            <div class="d-flex align-items-center mb-3">
-                                <div class="me-3">
-                                    <div class="profile-img-posts"></div>
-                                </div>
-                                <div class="profile">
-                                    <p class="fs-6 card-title m-0" id="profile-name">Jhon Louie <span
-                                            class="fs-6 fw-normal">@thejldeleon</span>
-                                    </p>
-                                    <p class="fs-6 card-text text-muted m-0">will post something</p>
-                                </div>
-
-                            </div>
-                            <p class="card-text">Brighten up your living space with a pop of color! Incorporating a bold
-                                accent wall or statement piece can elevate any
-                                room's design. #interiordesign #homedecor #colorinspiration</p>
-                            <a href="#" class="btn btn-comment">
-                                <i class='bx bx-message-rounded me-1 fs-6'></i>
-                                <span class="fs-6 fw-normal">Comment</span>
-                            </a>
-                        </div>
+                    <div class="profile">
+                        <p class="fs-6 card-title m-0" id="profile-name">Jhon Louie <span
+                                class="fs-6 fw-normal">@thejldeleon</span>
+                        </p>
+                        <p class="fs-6 card-text text-muted m-0">will post something</p>
                     </div>
                 </div>
+                <div class="card-body">
+                    <p class="card-title fs-5 mb-4">Title of post</p>
+                </div>
+                <img src="{{asset('/images/home-images/home-2.png')}}" class="img-fluid mb-4" alt="...">
+                <div class="card-body">
+                    <p class="card-text">
+                        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut
+                        labore et dolore magna
+                        aliqua. Duis at consectetur lorem donec. Dolor magna eget est lorem. Vel eros donec ac odio
+                        tempor. Volutpat odio
+                        facilisis mauris sit amet massa vitae tortor.
+                    </p>
+                </div>
+                <a href="#" class="btn btn-comment mt-3">
+                    <i class='bx bx-message-rounded me-1 fs-6'></i>
+                    <span class="fs-6 fw-normal">Comment</span>
+                </a>
             </div>
-            <div class="posts-card card mb-3">
-                <div class="row g-0">
-                    <div class="col-md-3 p-3 my-auto">
-                        <img src="{{asset('/images/home-images/home-1.png')}}" alt="" class="img-fluid">
+            <div class="posts p-3 mb-4 mt-md-0 mt-4">
+                <div class="d-flex align-items-center mb-3">
+                    <div class="me-3">
+                        <img src="{{asset('/images/img-placeholder.png')}}" alt="" class="profile-img-posts img-fluid">
                     </div>
-                    <div class="col-md-7">
-                        <div class="card-body">
-                            <div class="d-flex align-items-center mb-3">
-                                <div class="me-3">
-                                    <div class="profile-img-posts"></div>
-                                </div>
-                                <div class="profile">
-                                    <p class="fs-6 card-title m-0" id="profile-name">Jhon Louie <span
-                                            class="fs-6 fw-normal">@thejldeleon</span>
-                                    </p>
-                                    <p class="fs-6 card-text text-muted m-0">will post something</p>
-                                </div>
-
-                            </div>
-                            <p class="card-text">Brighten up your living space with a pop of color! Incorporating a bold
-                                accent wall or statement piece can elevate any
-                                room's design. #interiordesign #homedecor #colorinspiration</p>
-                            <a href="#" class="btn btn-comment">
-                                <i class='bx bx-message-rounded me-1 fs-6'></i>
-                                <span class="fs-6 fw-normal">Comment</span>
-                            </a>
-                        </div>
+                    <div class="profile">
+                        <p class="fs-6 card-title m-0" id="profile-name">Jhon Louie <span
+                                class="fs-6 fw-normal">@thejldeleon</span>
+                        </p>
+                        <p class="fs-6 card-text text-muted m-0">will post something</p>
                     </div>
                 </div>
+                <div class="card-body">
+                    <p class="card-title fs-5 mb-4">Title of post</p>
+                </div>
+                <img src="{{asset('/images/home-images/home-2.png')}}" class="img-fluid mb-4" alt="...">
+                <div class="card-body">
+                    <p class="card-text">
+                        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut
+                        labore et dolore magna
+                        aliqua. Duis at consectetur lorem donec. Dolor magna eget est lorem. Vel eros donec ac odio
+                        tempor. Volutpat odio
+                        facilisis mauris sit amet massa vitae tortor.
+                    </p>
+                </div>
+                <a href="#" class="btn btn-comment mt-3">
+                    <i class='bx bx-message-rounded me-1 fs-6'></i>
+                    <span class="fs-6 fw-normal">Comment</span>
+                </a>
             </div>
-            <div class="posts-card card mb-3">
-                <div class="row g-0">
-                    <div class="col-md-3 p-3 my-auto">
-                        <img src="{{asset('/images/home-images/home-1.png')}}" alt="" class="img-fluid">
+            <div class="posts p-3 mb-4">
+                <div class="d-flex align-items-center mb-3">
+                    <div class="me-3">
+                        <img src="{{asset('/images/img-placeholder.png')}}" alt="" class="profile-img-posts img-fluid">
                     </div>
-                    <div class="col-md-7">
-                        <div class="card-body">
-                            <div class="d-flex align-items-center mb-3">
-                                <div class="me-3">
-                                    <div class="profile-img-posts"></div>
-                                </div>
-                                <div class="profile">
-                                    <p class="fs-6 card-title m-0" id="profile-name">Jhon Louie <span
-                                            class="fs-6 fw-normal">@thejldeleon</span>
-                                    </p>
-                                    <p class="fs-6 card-text text-muted m-0">will post something</p>
-                                </div>
-
-                            </div>
-                            <p class="card-text">Brighten up your living space with a pop of color! Incorporating a bold
-                                accent wall or statement piece can elevate any
-                                room's design. #interiordesign #homedecor #colorinspiration</p>
-                            <a href="#" class="btn btn-comment">
-                                <i class='bx bx-message-rounded me-1 fs-6'></i>
-                                <span class="fs-6 fw-normal">Comment</span>
-                            </a>
-                        </div>
+                    <div class="profile">
+                        <p class="fs-6 card-title m-0" id="profile-name">Jhon Louie <span
+                                class="fs-6 fw-normal">@thejldeleon</span>
+                        </p>
+                        <p class="fs-6 card-text text-muted m-0">will post something</p>
                     </div>
                 </div>
-            </div>
-            <div class="posts-card card mb-3">
-                <div class="row g-0">
-                    <div class="col-md-3 p-3 my-auto">
-                        <img src="{{asset('/images/home-images/home-1.png')}}" alt="" class="img-fluid">
-                    </div>
-                    <div class="col-md-7">
-                        <div class="card-body">
-                            <div class="d-flex align-items-center mb-3">
-                                <div class="me-3">
-                                    <div class="profile-img-posts"></div>
-                                </div>
-                                <div class="profile">
-                                    <p class="fs-6 card-title m-0" id="profile-name">Jhon Louie <span
-                                            class="fs-6 fw-normal">@thejldeleon</span>
-                                    </p>
-                                    <p class="fs-6 card-text text-muted m-0">will post something</p>
-                                </div>
-
-                            </div>
-                            <p class="card-text">Brighten up your living space with a pop of color! Incorporating a bold
-                                accent wall or statement piece can elevate any
-                                room's design. #interiordesign #homedecor #colorinspiration</p>
-                            <a href="#" class="btn btn-comment">
-                                <i class='bx bx-message-rounded me-1 fs-6'></i>
-                                <span class="fs-6 fw-normal">Comment</span>
-                            </a>
-                        </div>
-                    </div>
+                <div class="card-body">
+                    <p class="card-title fs-5 mb-4">Title of post</p>
                 </div>
-            </div>
-            <div class="posts-card card mb-3">
-                <div class="row g-0">
-                    <div class="col-md-3 p-3 my-auto">
-                        <img src="{{asset('/images/home-images/home-1.png')}}" alt="" class="img-fluid">
-                    </div>
-                    <div class="col-md-7">
-                        <div class="card-body">
-                            <div class="d-flex align-items-center mb-3">
-                                <div class="me-3">
-                                    <div class="profile-img-posts"></div>
-                                </div>
-                                <div class="profile">
-                                    <p class="fs-6 card-title m-0" id="profile-name">Jhon Louie <span
-                                            class="fs-6 fw-normal">@thejldeleon</span>
-                                    </p>
-                                    <p class="fs-6 card-text text-muted m-0">will post something</p>
-                                </div>
-
-                            </div>
-                            <p class="card-text">Brighten up your living space with a pop of color! Incorporating a bold
-                                accent wall or statement piece can elevate any
-                                room's design. #interiordesign #homedecor #colorinspiration</p>
-                            <a href="#" class="btn btn-comment">
-                                <i class='bx bx-message-rounded me-1 fs-6'></i>
-                                <span class="fs-6 fw-normal">Comment</span>
-                            </a>
-                        </div>
-                    </div>
+                <img src="{{asset('/images/home-images/home-2.png')}}" class="img-fluid mb-4" alt="...">
+                <div class="card-body">
+                    <p class="card-text">
+                        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut
+                        labore et dolore magna
+                        aliqua. Duis at consectetur lorem donec. Dolor magna eget est lorem. Vel eros donec ac odio
+                        tempor. Volutpat odio
+                        facilisis mauris sit amet massa vitae tortor.
+                    </p>
                 </div>
-            </div>
-            <div class="posts-card card mb-3">
-                <div class="row g-0">
-                    <div class="col-md-3 p-3 my-auto">
-                        <img src="{{asset('/images/home-images/home-1.png')}}" alt="" class="img-fluid">
-                    </div>
-                    <div class="col-md-7">
-                        <div class="card-body">
-                            <div class="d-flex align-items-center mb-3">
-                                <div class="me-3">
-                                    <div class="profile-img-posts"></div>
-                                </div>
-                                <div class="profile">
-                                    <p class="fs-6 card-title m-0" id="profile-name">Jhon Louie <span
-                                            class="fs-6 fw-normal">@thejldeleon</span>
-                                    </p>
-                                    <p class="fs-6 card-text text-muted m-0">will post something</p>
-                                </div>
-
-                            </div>
-                            <p class="card-text">Brighten up your living space with a pop of color! Incorporating a bold
-                                accent wall or statement piece can elevate any
-                                room's design. #interiordesign #homedecor #colorinspiration</p>
-                            <a href="#" class="btn btn-comment">
-                                <i class='bx bx-message-rounded me-1 fs-6'></i>
-                                <span class="fs-6 fw-normal">Comment</span>
-                            </a>
-                        </div>
-                    </div>
-                </div>
+                <a href="#" class="btn btn-comment mt-3">
+                    <i class='bx bx-message-rounded me-1 fs-6'></i>
+                    <span class="fs-6 fw-normal">Comment</span>
+                </a>
             </div>
         </div>
     </div>
