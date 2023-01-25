3d8abf2744f49fb40c4f484cd16d699330a6ffe3
dumpit-ca <dumpitca@outlook.com>
2023-01-25 16:15:40 +0800

    fixed register page on mobile

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -8,40 +8,40 @@
         </div>
         <div class="col-lg-5">
             <form class="row p-5 p-md-5" data-bitwarden-watching="1">
-                <h1 class="display-6 mb-4 register-header">Register Account</h1>
-                <div class="col-md-6 mb-4">
+                <h1 class="display-6 mb-3 register-header">Register Account</h1>
+                <div class="col-md-6 mb-3">
                     <label for="inputFirstName" class="form-label">First Name</label>
                     <input type="text" class="form-control" placeholder="First Name">
                 </div>
-                <div class="col-md-6 mb-4">
+                <div class="col-md-6 mb-3">
                     <label for="inputLastName" class="form-label">Last Name</label>
                     <input type="text" class="form-control" placeholder="Last Name">
                 </div>
-                <div class="col-md-6 mb-4">
+                <div class="col-md-6 mb-3">
                     <label for="inputUsername" class="form-label">Username</label>
                     <input type="text" class="form-control" placeholder="Username">
                 </div>
-                <div class="col-md-6 mb-4">
+                <div class="col-md-6 mb-3">
                     <label for="inputNumber" class="form-label">Contact Number</label>
                     <input type="text" class="form-control" placeholder="Contact Number">
                 </div>
-                <div class="col-12 mb-4">
+                <div class="col-12 mb-3">
                     <label for="inputEmailAddress" class="form-label">Email Address</label>
                     <input type="text" class="form-control" placeholder="Email Address">
                 </div>
-                <div class="col-12 mb-4">
+                <div class="col-12 mb-3">
                     <label for="inputAddress" class="form-label">Address</label>
                     <input type="text" class="form-control" placeholder="Address">
                 </div>
-                <div class="col-md-6 mb-4">
+                <div class="col-md-6 mb-3">
                     <label for="inputUsername" class="form-label">Password</label>
                     <input type="text" class="form-control" placeholder="Username">
                 </div>
-                <div class="col-md-6 mb-4">
+                <div class="col-md-6 mb-3">
                     <label for="inputNumber" class="form-label">Repeat Password:</label>
                     <input type="text" class="form-control" placeholder="Contact Number">
                 </div>
-                <div class="mb-4 form-check">
+                <div class="mb-3 form-check">
                     <input type="checkbox" class="form-check-input" id="exampleCheck1">
                     <label class="form-check-label" for="exampleCheck1">
                         I agree to the website's
