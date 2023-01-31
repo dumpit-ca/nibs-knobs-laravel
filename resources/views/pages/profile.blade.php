03594ede44046747f0c51f519577926b4553ac68
dumpit-ca <dumpitca@outlook.com>
2023-01-31 15:43:24 +0800

    added comments

--- resources/views/pages/profile.blade.php
+++ resources/views/pages/profile.blade.php
@@ -7,8 +7,8 @@
             <aside class="profile-card card border-0">
                 <img src="{{asset('/images/home-images/profile-header.png')}}" class="card-img-top" alt="...">
                 <div class="position-relative mx-auto d-block mb-5">
-                    <img src="{{ $user->image ? '/uploads/user/'. $user->image : asset('/images/img-placeholder.png') }}" alt=""
-                        class="profile-img position-absolute top-50 start-0 translate-middle">
+                    <img src="{{ $user->image ? '/uploads/user/'. $user->image : asset('/images/img-placeholder.png') }}"
+                        alt="" class="profile-img position-absolute top-50 start-0 translate-middle">
                 </div>
                 <div class="card-body text-center">
                     <h5 class="profile-name card-title">{{ $user->first_name }}</h5>
@@ -25,8 +25,8 @@
                 <div class="d-flex align-items-center mb-3">
                     @php $poster = App\User::find($post->user_id) @endphp
                     <div class="me-3">
-                        <img src="{{ $poster->image ? '/uploads/user/'. $poster->image : asset('/images/img-placeholder.png') }}" alt="user profile image"
-                            class="profile-img-posts img-fluid">
+                        <img src="{{ $poster->image ? '/uploads/user/'. $poster->image : asset('/images/img-placeholder.png') }}"
+                            alt="user profile image" class="profile-img-posts img-fluid">
                     </div>
                     <div class="profile me-auto">
                         <p class="fs-6 card-title m-0" id="profile-name">{{ $poster->first_name }} <span
@@ -40,7 +40,8 @@
                         <i class="bx bx-dots-horizontal-rounded btn-icon fs-4" data-bs-toggle="dropdown"
                             aria-expanded="false"></i>
                         <ul class="dropdown-menu">
-                            <li><a class="dropdown-item" href="{{ route('post.delete', [$post->id]) }}">Delete Post</a></li>
+                            <li><a class="dropdown-item" href="{{ route('post.delete', [$post->id]) }}">Delete Post</a>
+                            </li>
                         </ul>
                     </div>
                     @endif
@@ -56,6 +57,11 @@
                 <form action="{{ route('comment.create', ['id'=> $post->id]) }}" method="POST" class="mt-4">
                     @csrf
                     <div class="d-flex align-items-center gap-3">
+                        <a href="{{route('post.show', ['id'=> $post->id])}}"
+                            class="btn view-comment fs-6 d-flex align-items-center" type="button">
+                            <i class='bx bx-message-rounded me-1 fs-6'></i>
+                            Comments
+                        </a>
                         <div class="my-3 w-100">
                             <input type="text" name="content" class="form-control" placeholder="Add a comment...">
                         </div>
