b3023bf6c0b537d98692c662bc9c3a6231562d1c
dumpit-ca <dumpitca@outlook.com>
2023-01-31 15:46:15 +0800

    removed profile img on comment section

--- resources/views/pages/posts.blade.php
+++ resources/views/pages/posts.blade.php
@@ -7,7 +7,7 @@
             <aside class="profile-card card border-0">
                 <img src="{{asset('/images/home-images/profile-header.png')}}" class="card-img-top" alt="...">
                 <div class="position-relative mx-auto d-block mb-5">
-                    <img src="{{ $user->image ? '/uploads/user/'. $user->image : asset('/images/img-placeholder.png') }}" alt=""
+                    <img src="{{asset('/images/img-placeholder.png')}}" alt=""
                         class="profile-img position-absolute top-50 start-0 translate-middle">
                 </div>
                 <div class="card-body text-center">
@@ -22,7 +22,7 @@
             <div class="create-posts p-4 mb-4 mt-md-0 mt-4">
                 <div class="d-flex align-items-center mb-3">
                     <div class="me-3">
-                        <img src="{{ $user->image ? '/uploads/user/'. $user->image : asset('/images/img-placeholder.png') }}" alt="" class="profile-img-posts img-fluid">
+                        <img src="{{asset('/images/img-placeholder.png')}}" alt="" class="profile-img-posts img-fluid">
                     </div>
                     <div class="profile">
                         <p class="fs-6 card-title m-0" id="profile-name">{{ $user->first_name }} <span
@@ -31,31 +31,32 @@
                         <p class="fs-6 card-text text-muted m-0">will post something</p>
                     </div>
                 </div>
-               <form action="{{ route('post.create') }}" method="POST" enctype="multipart/form-data">
-                @csrf
-                        <div class="form-floating mb-3">
-                            <input type="file" name="image" class="form-control" id="floatingInput" >
-                            <label for="floatingInput">Post Image</label>
-                            <span class="badge badge-danger w-100 validation-message">{{$errors->first('image')}}</span>
-                        </div>
-                        <div class="form-floating mb-3">
-                            <input type="text" name="title" class="form-control" id="floatingInput" placeholder="Post Title">
-                            <label for="floatingInput">Post Title</label>
-                            <span class="badge badge-danger w-100 validation-message">{{$errors->first('title')}}</span>
-                        </div>
-                        <div class=" form-floating mb-3">
-                            <select id="select" name="category" class="form-control form-select">
-                                <option value="" selected>Choose a Category</option>
-                                <option value="Bedroom">Bedroom</option>
-                                <option value="Kitchen">Kitchen</option>
-                                <option value="Living Room">Living Room</option>
-                                <option value="Bathroom">Bathroom</option>
-                                <option value="Space Saving">Space Saving</option>
-                                <option value="Home Office">Home Office</option>
-                              </select>
-                            <label for="select">Category</label>
-                            <span class="badge badge-danger w-100 validation-message">{{$errors->first('category')}}</span>
-                        </div>
+                <form action="{{ route('post.create') }}" method="POST" enctype="multipart/form-data">
+                    @csrf
+                    <div class="form-floating mb-3">
+                        <input type="file" name="image" class="form-control" id="floatingInput">
+                        <label for="floatingInput">Post Image</label>
+                        <span class="badge badge-danger w-100 validation-message">{{$errors->first('image')}}</span>
+                    </div>
+                    <div class="form-floating mb-3">
+                        <input type="text" name="title" class="form-control" id="floatingInput"
+                            placeholder="Post Title">
+                        <label for="floatingInput">Post Title</label>
+                        <span class="badge badge-danger w-100 validation-message">{{$errors->first('title')}}</span>
+                    </div>
+                    <div class=" form-floating mb-3">
+                        <select id="select" name="category" class="form-control form-select">
+                            <option value="" selected>Choose a Category</option>
+                            <option value="Bedroom">Bedroom</option>
+                            <option value="Kitchen">Kitchen</option>
+                            <option value="Living Room">Living Room</option>
+                            <option value="Bathroom">Bathroom</option>
+                            <option value="Space Saving">Space Saving</option>
+                            <option value="Home Office">Home Office</option>
+                        </select>
+                        <label for="select">Category</label>
+                        <span class="badge badge-danger w-100 validation-message">{{$errors->first('category')}}</span>
+                    </div>
                     <div class="my-4">
                         <textarea name="content" class="form-control"></textarea>
                     </div>
@@ -70,12 +71,12 @@
 
             <div class="posts p-3 mb-4 mt-md-0 mt-4">
                 <div class="d-flex align-items-center mb-3">
-                    @php $poster = App\User::find($post->user_id) @endphp
                     <div class="me-3">
-                        <img src="{{ $poster->image ? '/uploads/user/'. $poster->image : asset('/images/img-placeholder.png') }}" alt="user profile image"
+                        <img src="{{asset('/images/img-placeholder.png')}}" alt="user profile image"
                             class="profile-img-posts img-fluid">
                     </div>
                     <div class="profile me-auto">
+                        @php $poster = App\User::find($post->user_id) @endphp
                         <p class="fs-6 card-title m-0" id="profile-name">{{ $poster->first_name }} <span
                                 class="fs-6 fw-normal">&#64;{{ $poster->username }} </span>
                         </p>
@@ -87,7 +88,8 @@
                         <i class="bx bx-dots-horizontal-rounded btn-icon fs-4" data-bs-toggle="dropdown"
                             aria-expanded="false"></i>
                         <ul class="dropdown-menu">
-                            <li><a class="dropdown-item" href="{{ route('post.delete',['id' => $post->id]) }}">Delete Post</a></li>
+                            <li><a class="dropdown-item" href="{{ route('post.delete',['id' => $post->id]) }}">Delete
+                                    Post</a></li>
                         </ul>
                     </div>
                     @endif
@@ -103,20 +105,51 @@
                 <form action="{{ route('comment.create', ['id'=> $post->id]) }}" method="POST" class="mt-4">
                     @csrf
                     <div class="d-flex align-items-center gap-3">
-
+                        {{-- see comments buttons --}}
+                        <a href="{{route('post.show', ['id'=> $post->id])}}"
+                            class="btn view-comment fs-6 d-flex align-items-center" type="button">
+                            <i class='bx bx-message-rounded me-1 fs-6'></i>
+                            Comments
+                        </a>
                         <div class="my-3 w-100">
                             <input type="text" name="content" class="form-control" placeholder="Add a comment...">
                         </div>
                         <div class="">
                             <button type="submit" class="btn btn-comment"><i class='bx bx-send'></i></button>
                         </div>
-                    </div>
 
+
+                    </div>
+                    {{-- comments here --}}
+                    <div class="collapse my-3" id="collapseExample">
+                        <div class="comment p-3">
+                            <div class="d-flex align-items-center mb-3">
+                                <div class="me-3">
+                                    <img src="{{asset('/images/img-placeholder.png')}}" alt=""
+                                        class="profile-img-posts img-fluid">
+                                </div>
+                                <div class="profile">
+                                    <p class="fs-6 card-title m-0" id="profile-name">Cass <span
+                                            class="fs-6 fw-normal">@cassoy</span>
+                                    </p>
+                                    <p class="fs-6 card-text text-muted m-0">10 hours ago</p>
+                                </div>
+                            </div>
+                            <div class="mt-3">
+                                <p class="fs-6">Just finished redecorating my living room and I am absolutely in love
+                                    with the
+                                    outcome! Interior design is truly an art
+                                    form, it has the power to completely transform a space and make it feel like a whole
+                                    new
+                                    place.</p>
+                            </div>
+                        </div>
+                    </div>
                 </form>
+
             </div>
             @endforeach
         </div>
     </div>
 </div>
 @endsection
-
