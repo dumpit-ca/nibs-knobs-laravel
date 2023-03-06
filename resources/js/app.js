ddd0a346783ba01f299688bcdd42ce678eb69393
cass <dumpitca@outlook.com>
2023-03-06 18:06:32 +0800

    Fixed chart.js

--- resources/js/app.js
+++ resources/js/app.js
@@ -2,7 +2,7 @@ window.$ = window.jQuery = window.jquery = require("jquery");
 require("bootstrap");
 
 // SweetAlert2
-window.Swal = require("sweetalert2/dist/sweetalert2.all.min");
+const Swal = (window.Swal = require("sweetalert2/dist/sweetalert2.all.min"));
 // Boxicons
 require("boxicons");
-// window.Chart = require("chart.js/auto").default;
+const Chart = (window.Chart = require("chart.js/auto"));
