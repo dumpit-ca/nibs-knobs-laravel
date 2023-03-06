45dcd0b94dc79294c71beb6d39a65e252555dd4c
cass <dumpitca@outlook.com>
2023-03-06 18:36:56 +0800

    Installed Font Awesome via npm

--- resources/js/app.js
+++ resources/js/app.js
@@ -5,4 +5,9 @@ require("bootstrap");
 const Swal = (window.Swal = require("sweetalert2/dist/sweetalert2.all.min"));
 // Boxicons
 require("boxicons");
+// chart.js
 const Chart = (window.Chart = require("chart.js/auto"));
+// Font Awesome
+require("@fortawesome/fontawesome-free/js/regular.min.js");
+require("@fortawesome/fontawesome-free/js/brands.min.js");
+require("@fortawesome/fontawesome-free/js/fontawesome.min.js");
--- resources/views/layouts/admin/admin.blade.php
+++ resources/views/layouts/admin/admin.blade.php
@@ -5,7 +5,6 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
     <link rel="stylesheet" href="{{ asset('/css/admin.css') }}" nonce="{{ csp_nonce() }}">
-    <script src="https://kit.fontawesome.com/1d56e9de16.js" crossorigin="anonymous" nonce="{{ csp_nonce() }}"></script>
     <link rel="shortcut icon" href="{{ asset('/images/n&k-icon.png') }}" type="image/x-icon" nonce="{{ csp_nonce() }}">
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
