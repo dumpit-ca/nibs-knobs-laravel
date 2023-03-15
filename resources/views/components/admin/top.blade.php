7a7f50e227f23312afa52f6eb6051eeff0d75cf8
dumpit-ca <dumpitca@outlook.com>
2023-03-15 13:55:59 +0800

    Fixed admin name

--- resources/views/components/admin/top.blade.php
+++ resources/views/components/admin/top.blade.php
@@ -2,7 +2,9 @@
     <a class="dropdown-toggle text-decoration-none" href="#" role="button" data-bs-toggle="dropdown"
         aria-expanded="false">
         <i class='bx bxs-user'></i>
-        User
+        @php $user = Auth::user(); @endphp
+        {{-- User --}}
+        {{ Auth::user()->first_name  }}
     </a>
     <ul class="dropdown-menu">
         <li><a class="dropdown-item" href="{{route('logout')}}">Logout</a></li>
